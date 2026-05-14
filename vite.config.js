2aca9a2c918195dd70e33cc5a206b91470caecef
Ola Al Zreikat <oalzre180@west-mec.org>
2026-05-14 14:15:41 -0700

    another commit

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/ola-portfolio-react/",
+  base: "/ola-portfolio-react/",
 });
